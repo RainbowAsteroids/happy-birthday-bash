1e6a1db9be022e08e21b0d2d2bc55b9ad87298f1
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-11-04 16:04:44 -0400

    finish fan; fix #9

---
+++ Weapon/Fan/fan.gd
@@ -0,0 +1,20 @@
+extends Weapon
+
+@export var wind_force := 30.0
+@export var velocity_divisor := 100.0
+
+@onready var wind_hitbox := $WindHitbox as Area2D
+
+func _physics_process(_delta):
+	for body in wind_hitbox.get_overlapping_bodies():
+		if body is Enemy:
+			print(body)
+			var r_hat = (body.global_position - global_position).normalized()
+
+			var velocity_factor = linear_velocity.length() / velocity_divisor
+
+			body.apply_central_force(r_hat * wind_force * velocity_factor)
+
+func _on_weapon_collision_handler_enemy_hit(_enemy: Enemy):
+	durability -= 1
+
