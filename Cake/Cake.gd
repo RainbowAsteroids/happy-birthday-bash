5803849fa0dc8f8e3757d466350d4518fc89ba23
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-11-04 23:24:28 -0400

    cake art; ref #7

--- Cake/Cake.gd
+++ Cake/Cake.gd
@@ -4,15 +4,26 @@ static var instance: Cake
 
 @export var health := 100.0
 
+@export var happy_sprite: Sprite2D
+@export var scared_sprite: Sprite2D
+
+var being_attacked := false
+
 @onready var area := $Area2D as Area2D
 
 func _init():
 	Cake.instance = self
 
 func _physics_process(delta: float):
+	being_attacked = false
+
 	for body in area.get_overlapping_bodies():
 		if body is Enemy:
+			being_attacked = true
 			health -= body.damage * delta
 	
+	happy_sprite.visible = not being_attacked
+	scared_sprite.visible = being_attacked
+
 	if health <= 0:
 		print("ded")
