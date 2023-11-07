e827309d25b2dd8f6569e273709c6d48069a334a
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-11-07 13:15:34 -0500

    change to enemy spawner

--- Enemy/enemy_spawner.gd
+++ Enemy/enemy_spawner.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 @export var enemy_scene: PackedScene
 
+@export var minimum_respawn_time := 0.2
+var respawn_chance := 1.0
 @export var respawn_time := 1.:
 	set(value):
-		respawn_time = value
+		respawn_time = max(minimum_respawn_time, value)
+		respawn_chance += respawn_time - value # 0 if value >= minimum, positive if not
+
 		if timer != null:
 			timer.start(respawn_time)
 
@@ -13,16 +17,22 @@ extends Node2D
 @onready var timer = $SpawnTimer
 
 func _on_timer_timeout():
-	#print("timeout")
 	respawn_time -= randf() * 0.15
-	var enemy = enemy_scene.instantiate()
-	enemy.global_position = global_position
+
+	# respawn_chance can be higher than 1. if so, generate multiple enemies
+	# 2.35 = 100% chance + 100% chance + 35% chance
+	var n := respawn_chance
+	while randf() < n:
+		n -= 1.0
+		var enemy = enemy_scene.instantiate()
+		enemy.global_position = global_position
 	
-	enemy_parent.add_child(enemy)
+		enemy_parent.add_child(enemy)
 
 func _ready():
 	#print(respawn_time)
 	timer.autostart = true
+	timer.one_shot = true
 	timer.start(respawn_time)
 
 	if enemy_parent == null:
