02a788475b3f3c0ceab05c7bee2c7fa88b2f09cf
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-11-10 15:24:59 -0500

    fix code smell

--- Shop/gold.gd
+++ Shop/gold.gd
@@ -3,7 +3,6 @@ class_name Gold extends RigidBody2D
 signal collected
 
 @export var move_force := 50.
-@export var max_velocity := 75.
 
 func _on_mouse_entered():
 	collected.emit()
@@ -13,4 +12,3 @@ func _physics_process(_delta):
 	var to_mouse_hat = (MousePos.mouse_pos - global_position).normalized()
 	apply_central_force(to_mouse_hat * move_force)
 
-	linear_velocity = linear_velocity.limit_length(max_velocity)
